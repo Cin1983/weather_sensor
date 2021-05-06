e177ec02fadafeadb25837997c52d75672f86e9e
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-05-06 15:28:40 +0200

    statements

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-
-const SeasonDisplay = () => {
-    return(<div>Hello girl!</div>)
-}
-
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === "winter" ? "Burr, it is chilly" : "Lets hit the Beach";
+  
+    return (<div><h1>{text}</h1></div>
+    )};
 export default SeasonDisplay;
